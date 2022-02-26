79566e13e765c1868892586a8a0d0b619083f7a1
omadli <76625693+omadli@users.noreply.github.com>
2022-02-26 15:47:35 +0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-!5u=y$0fk9hu5!g8(^*epbzngvgbrz@d8d-^8orqb9&t$5^svm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
